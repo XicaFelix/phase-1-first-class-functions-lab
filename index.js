f24147995061f6fd658aa1b62e4e6d23db121905
Xica Felix <xicasfelix@gmail.com>
2021-11-23 20:31:43 -0500

    commit tree 497e19877fffbf217f23009e9ef941ad56b3e2aa

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(array){
+    let x= array.slice(0,2);
+    return x;
+}
+const returnLastTwoDrivers = function(array){
+    let y=array.slice(-2);
+    return y;
+}
+function selectingDrivers(array){
+    let newArr = [...returnFirstTwoDrivers,...returnLastTwoDrivers];
+    return newArr;
+}
